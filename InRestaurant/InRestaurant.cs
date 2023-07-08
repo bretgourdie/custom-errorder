7afaeb647367f199c9ff08d64024d4ad5cdf3c7f
Bret Gourdie <BretGourdie@gmail.com>
2023-07-08 15:22:20 -0400

    Use spaces instead of tabs

--- InRestaurant/InRestaurant.cs
+++ InRestaurant/InRestaurant.cs
@@ -3,43 +3,43 @@ using System;
 
 public partial class InRestaurant : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		findFoodChoiceButton(FoodChoice.Cheap).Pressed += foodChoiceCheapPressed;
-		findFoodChoiceButton(FoodChoice.Mid).Pressed += foodChoiceMidPressed;
-		findFoodChoiceButton(FoodChoice.Expensive).Pressed += foodChoiceExpensivePressed;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        findFoodChoiceButton(FoodChoice.Cheap).Pressed += foodChoiceCheapPressed;
+        findFoodChoiceButton(FoodChoice.Mid).Pressed += foodChoiceMidPressed;
+        findFoodChoiceButton(FoodChoice.Expensive).Pressed += foodChoiceExpensivePressed;
+    }
 
-	private Button findFoodChoiceButton(FoodChoice foodChoice)
-	{
-		return GetNode<Button>($"MenuBackground/FoodChoice{foodChoice}");
-	}
+    private Button findFoodChoiceButton(FoodChoice foodChoice)
+    {
+        return GetNode<Button>($"MenuBackground/FoodChoice{foodChoice}");
+    }
 
-	private void foodChoiceMidPressed()
-	{
-		GD.Print(nameof(foodChoiceMidPressed));
-	}
+    private void foodChoiceMidPressed()
+    {
+        GD.Print(nameof(foodChoiceMidPressed));
+    }
 
-	private void foodChoiceExpensivePressed()
-	{
-		GD.Print(nameof(foodChoiceExpensivePressed));
-	}
+    private void foodChoiceExpensivePressed()
+    {
+        GD.Print(nameof(foodChoiceExpensivePressed));
+    }
 
-	private void foodChoiceCheapPressed()
-	{
-		GD.Print(nameof(foodChoiceCheapPressed));
-	}
+    private void foodChoiceCheapPressed()
+    {
+        GD.Print(nameof(foodChoiceCheapPressed));
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	private enum FoodChoice
-	{
-		Cheap,
-		Mid,
-		Expensive
-	}
+    private enum FoodChoice
+    {
+        Cheap,
+        Mid,
+        Expensive
+    }
 }
